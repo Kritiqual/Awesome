5eb5b4d5fcd2609874af95807a9575ef040ab56f
Kritiqual <kritiqual.pw@gmail.com>
2022-07-12 17:42:53 +0700

    Add submodules

--- rc.lua
+++ rc.lua
@@ -15,7 +15,7 @@ naughty.connect_signal("request::display_error", function(message, startup)
 end)
 --<~>--
 local beautiful = require("beautiful")
-beautiful.init("/home/kritiqual/.config/awesome/themes/theme.lua")
+beautiful.init(os.getenv("HOME") .. "/.config/awesome/themes/theme.lua")
 --<~>--
 require("main")
 require("widgets")
